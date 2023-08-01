41ad485fb4b5caefcd2e115f257c5d737e90b510
Farzeen Zainab <farzeenzaidi51@gmail.com>
2023-08-01 07:19:42 +0500

    tagging functionality completed

--- components/base/Canvas.js
+++ components/base/Canvas.js
@@ -1,80 +1,98 @@
-import React, { useRef, forwardRef, useEffect, useState } from "react";
+import React, {
+  useRef,
+  forwardRef,
+  useEffect,
+  useState,
+  useCallback,
+} from "react";
 import styles from "../styles/canvas.module.css";
 
 const Canvas = forwardRef(function ({ height, width }, ref) {
-  const canvas = useRef();
-  const [context, setContext] = useState(null);
-  const [mousePos, setMousePos] = useState({ x: null, y: null });
-  const [start, setStart] = useState({ x: null, y: null });
-  const [isActive, setIsActive] = useState(false);
-  const [dimension, setDimension] = useState({ width: "0px", height: "0px" });
-  const [isMirrored, setIsMirrored] = useState({ x: false, y: false });
+  const canvasRef = useRef(null);
+  const [start, setStart] = useState({});
+  const [tags, setTags] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
-    setContext(canvas.current.getContext("2d"));
+    const canvas = canvasRef.current;
+    canvas.getContext("2d");
   }, []);
 
-  const getMousePos = (e) => {
-    const rect = canvas.current.getBoundingClientRect();
+  const getMousePos = (canvas, e) => {
+    // gives dimensions of the canvas relative to the viewport, which allows us to calculate the correct mouse position on the account taking into account any scaling or resizing the canvas
+    const rect = canvas.getBoundingClientRect();
     return {
-      x: ((e.clientX - rect.left) * width) / rect.width,
-      y: ((e.clientY - rect.top) * height) / rect.height,
+      x: ((e.clientX - rect.left) * canvas.width) / rect.width,
+      y: ((e.clientY - rect.top) * canvas.height) / rect.height,
     };
   };
 
-  const startRect = (e) => {
-    setIsActive(true);
-    setStart(getMousePos(e));
-    setMousePos(getMousePos(e));
-  };
+  // starting action
+  function startRect(e) {
+    setIsDrawing(true);
+    setStart(getMousePos(canvasRef.current, e));
+  }
 
-  const endRect = (e) => {
-    setStart({ x: null, y: null });
-    setDimension({ width: "0px", height: "0px" });
-    setIsActive(false);
-  };
+  // ending action
+  function endRect(e) {
+    if (isDrawing) {
+      let { x, y } = getMousePos(canvasRef.current, e);
+      setTags([
+        ...tags,
+        { x: start.x, y: start.y, width: x - start.x, height: y - start.y },
+      ]);
+      setIsDrawing(false);
+    }
+  }
 
+  // draw rectangle
+  // This is called to render all the drawn rectangle on the canvas
+  function draw() {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    // clear everthing from the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // render all tags
+    tags.forEach((tag) => {
+      context.beginPath();
+      context.rect(tag.x, tag.y, tag.width, tag.height);
+      context.strokeStyle = "blue";
+      context.stroke();
+    });
+  }
+
+  // moving action
   const handleMouseMove = (e) => {
-    if (start.x) {
-      let { x, y } = getMousePos(e);
-      let width = x - start.x;
-      let height = y - start.y;
-
-      let xMirror = width < 0;
-      let yMirror = height < 0;
-
-      setIsMirrored({ x: xMirror, y: yMirror });
-
-      if (xMirror) {
-        x = start.x + width;
-      }
-
-      if (yMirror) {
-        y = start.y + height;
-      }
-
-      setDimension({ width: Math.abs(width) + "px", height: Math.abs(height) + "px" });
-      setMousePos({ x, y });
+    if (isDrawing) {
+      draw();
+      let { x, y } = getMousePos(canvasRef.current, e);
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+      context.beginPath();
+      context.rect(start.x, start.y, x - start.x, y - start.y);
+      context.strokeStyle = "red";
+      context.stroke();
     }
   };
 
+  // add tag name
+
+  // delete tag
+
   return (
     <>
-      {isActive && (
-        <div
-          className={styles.rectangle}
-          style={{
-            position: "absolute",
-            left: isMirrored.x ? start.x - parseInt(dimension.width) : start.x,
-            top: isMirrored.y ? start.y - parseInt(dimension.height) : start.y,
-            width: dimension.width,
-            height: dimension.height,
-            backgroundColor: "rgba(255, 0, 0, 0.5)", // Transparent red
-            transform: `scaleX(${isMirrored.x ? -1 : 1}) scaleY(${isMirrored.y ? -1 : 1})`, // Apply mirroring
-          }}
-        ></div>
-      )}
-      <canvas ref={canvas} id="canvas" width={width} height={height} className={`${styles.canvas}`} onMouseMove={handleMouseMove} onMouseDown={startRect} onMouseUp={endRect}></canvas>
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        width={width}
+        height={height}
+        className={`${styles.canvas}`}
+        onMouseMove={handleMouseMove}
+        onMouseDown={startRect}
+        onMouseUp={endRect}
+      ></canvas>
     </>
   );
 });
